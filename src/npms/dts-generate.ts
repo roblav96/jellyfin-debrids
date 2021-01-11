d5403d1bd82d6c03782df9ffa5c7c105bbfdac06
Robert Laverty <roblav96@gmail.com>
2021-01-11 12:47:26 -0500

    Delete dts-generate.ts

--- src/npms/dts-generate.ts
+++
@@ -1,8 +0,0 @@
-// import * as dts from 'https://cdn.skypack.dev/dts-generate'
-// import dts from 'https://dev.jspm.io/dts-generate'
-// import generate from 'https://dev.jspm.io/dts-generate'
-// import dts from 'https://dev.jspm.io/dts-generate'
-// export default dts
-
-// @deno-types='../../node_modules/dts-generate/dist/index.d.ts'
-export { default } from 'https://dev.jspm.io/dts-generate'
