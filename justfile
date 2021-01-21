5643f49076446586352d2c6b7fc74488d2f01181
Robert Laverty <roblav96@gmail.com>
2021-01-21 06:51:50 -0500

    cleanup config files

--- justfile
+++ justfile
@@ -4,76 +4,53 @@
 _default :
 	@just --dump | bat --style=grid --language=make
 
+
+
 export ROOT_PATH := justfile_directory()
+export GRAYLOG_PORT := env_var_or_default("GRAYLOG_PORT", "18066")
 export JELLYFIN_CACHE_DIR := env_var_or_default("JELLYFIN_CACHE_DIR", justfile_directory() + "/.local/jellyfin/cache")
 export JELLYFIN_CONFIG_DIR := env_var_or_default("JELLYFIN_CONFIG_DIR", justfile_directory() + "/.local/jellyfin/config")
 export JELLYFIN_DATA_DIR := env_var_or_default("JELLYFIN_DATA_DIR", justfile_directory() + "/.local/jellyfin/data")
 export JELLYFIN_LOG_DIR := env_var_or_default("JELLYFIN_LOG_DIR", justfile_directory() + "/.local/jellyfin/data/log")
-export JELLYFIN_PORTABLE_DIR := env_var_or_default("JELLYFIN_PORTABLE_DIR", justfile_directory() + "/.local/jellyfin/portable")
+
+
 
 install :
 	deno cache --unstable --no-check --reload src/**/*.ts
-# ls -laph "{{JELLYFIN_PORTABLE_DIR}}/jellyfin"
 
-deps main="src/mod.ts" :
+deps main='src/mod.ts':
 	NO_COLOR=1 deno info --unstable "{{main}}"
+run main='src/mod.ts' :
+	deno cache --unstable --no-check src/**/*.ts || true
+	deno run --unstable --no-check --allow-all "{{main}}"
+watch main='src/mod.ts' :
+	watchexec --restart --no-default-ignore --watch=configs --watch=src -- 'tput clear; echo; just run {{main}}'
 
-run main="src/mod.ts" action="" :
-	@test "{{action}}" = "watch" && tput clear || true
-	@deno cache --unstable --no-check src/**/*.ts || true
-	@deno run --unstable --no-check --allow-all "{{main}}"
 
-watch main="src/mod.ts" :
-	watchexec --restart --watch configs --watch src -- just run "{{main}}" watch
 
-export GRAYLOG_PORT := env_var_or_default("GRAYLOG_PORT", "18066")
 jellyfin :
-	@mkdir -p "{{JELLYFIN_CONFIG_DIR}}"
-	@cp -f "{{ROOT_PATH}}/configs/jellyfin/logging.json" "{{JELLYFIN_CONFIG_DIR}}/logging.json"
-	jellyfin --service
-# @cp -f "{{ROOT_PATH}}/configs/jellyfin/logging.json" "{{JELLYFIN_CONFIG_DIR}}/logging.default.json"
-# @mkdir -p "{{JELLYFIN_LOG_DIR}}"
-# @truncate -s0 "{{JELLYFIN_LOG_DIR}}/debrids.log"
-# websocat -v --exit-on-eof --text ws-l:127.0.0.1:{{WEBSOCAT_PORT}} reuse-broadcast:mirror:cmd:'jellyfin --service'
+	mkdir -p -v "{{JELLYFIN_CONFIG_DIR}}"
+	cp -n -v "{{ROOT_PATH}}/configs/jellyfin/system.xml" "{{JELLYFIN_CONFIG_DIR}}/system.xml"
+	cp -f -v "{{ROOT_PATH}}/configs/jellyfin/logging.json" "{{JELLYFIN_CONFIG_DIR}}/logging.json"
+	jellyfin --service | bat --style=grid --language=yaml
+jellyfin-watch :
+	watchexec --restart --no-default-ignore --watch=configs/jellyfin -- 'tput clear; echo; just jellyfin'
+
+
+
+h2o :
+	h2o --conf "{{ROOT_PATH}}/configs/h2o.conf" --test
+	h2o --conf "{{ROOT_PATH}}/configs/h2o.conf" | bat --style=grid --language=yaml
+h2o-watch :
+	watchexec --restart --no-default-ignore --watch=configs/h2o.conf -- 'tput clear; echo; just h2o'
 
 nghttpx :
-	nghttpx --conf="{{ROOT_PATH}}/configs/nghttpx.dev.conf" --workers=`nproc`
+	nghttpx --conf="{{ROOT_PATH}}/configs/nghttpx.conf" --workers=`nproc`
+nghttpx-watch :
+	watchexec --restart --no-default-ignore --watch=configs/nghttpx.conf -- 'tput clear; echo; just nghttpx'
 
 nginx :
 	nginx -e "/dev/stderr" -c "{{ROOT_PATH}}/configs/nginx.conf" -T
 	nginx -e "/dev/stderr" -c "{{ROOT_PATH}}/configs/nginx.conf"
-
-
-
-# jellyfin release="10.7.0~rc2" :
-# 	@test ! -d "{{JELLYFIN_PORTABLE_DIR}}" \
-# 		&& mkdir -p "{{JELLYFIN_PORTABLE_DIR}}" \
-# 		&& wget "https://repo.jellyfin.org/releases/server/portable/versions/stable/combined/{{release}}/jellyfin_{{release}}.tar.gz" -O "{{JELLYFIN_PORTABLE_DIR}}/jellyfin_{{release}}.tar.gz" \
-# 		&& tar -xf "{{JELLYFIN_PORTABLE_DIR}}/jellyfin_{{release}}.tar.gz" --directory="{{JELLYFIN_PORTABLE_DIR}}" \
-# 		&& rm -f "{{JELLYFIN_PORTABLE_DIR}}/jellyfin_{{release}}.tar.gz" \
-# 		&& mv -f "{{JELLYFIN_PORTABLE_DIR}}/jellyfin_{{release}}" "{{JELLYFIN_PORTABLE_DIR}}/jellyfin" \
-# 		|| true
-# 	cp -f "{{ROOT_PATH}}/configs/jellyfin/logging.json" "{{JELLYFIN_CONFIG_DIR}}/logging.json"
-# 	dotnet "{{JELLYFIN_PORTABLE_DIR}}/jellyfin/jellyfin.dll" --service
-# 	# cp -f "{{ROOT_PATH}}/configs/jellyfin/logging.json" "{{JELLYFIN_CONFIG_DIR}}/logging.default.json"
-# 	# websocat --exit-on-eof --text ws-l:127.0.0.1:8080 broadcast:sh-c:'dotnet "{{JELLYFIN_PORTABLE_DIR}}/jellyfin/jellyfin.dll" --service'
-# 	# websocat --exit-on-eof --text ws-l:127.0.0.1:8080 broadcast:sh-c:"jellyfin --service"
-# # test ! -e "{{JELLYFIN_PORTABLE_DIR}}/jellyfin_{{release}}.tar.gz" \
-# # 	&& wget "https://repo.jellyfin.org/releases/server/portable/versions/stable/combined/{{release}}/jellyfin_{{release}}.tar.gz" -O "{{JELLYFIN_PORTABLE_DIR}}/jellyfin_{{release}}.tar.gz" \
-# # 	|| true
-# # test -e "{{JELLYFIN_PORTABLE_DIR}}/jellyfin_{{release}}.tar.gz" \
-# # 	&& mv "{{JELLYFIN_PORTABLE_DIR}}/jellyfin_{{release}}" "{{JELLYFIN_PORTABLE_DIR}}/jellyfin" \
-# # 	|| true
-# # mkdir -p "{{JELLYFIN_CONFIG_DIR}}"
-
-
-
-# export DENO_DIR := justfile_directory() + "/node_modules/.cache/deno"
-# install :
-# 	rm -r -f "node_modules"
-# 	mkdir -p "{{DENO_DIR}}"
-# 	curl --silent "https://raw.githubusercontent.com/denoland/deno/master/cli/dts/lib.deno.worker.d.ts" --output "{{DENO_DIR}}/lib.deno.d.ts"
-# 	echo >> "{{DENO_DIR}}/lib.deno.d.ts"
-# 	deno types --unstable >> "{{DENO_DIR}}/lib.deno.d.ts"
-# 	cp "{{DENO_DIR}}/lib.deno.d.ts" "{{DENO_DIR}}/lib.deno.unstable.d.ts"
-# 	just reload
+nginx-watch :
+	watchexec --restart --no-default-ignore --watch=configs/nginx.conf -- 'tput clear; echo; just nginx'
--- src/workers/graylog_worker.ts
+++ src/workers/graylog_worker.ts
@@ -1,163 +1,47 @@
 await import('../devops/console.ts')
-import * as _io from 'https://deno.land/std/http/_io.ts'
 import * as async from 'https://deno.land/std/async/mod.ts'
-import * as http from 'https://deno.land/std/http/mod.ts'
 import * as io from 'https://deno.land/std/io/mod.ts'
-import * as R from '../shims/rambdax.ts'
-import * as textproto from 'https://deno.land/std/textproto/mod.ts'
-import * as utf8 from 'https://deno.land/std/encoding/utf8.ts'
-import * as thenable from 'https://deno.land/x/thenable_generator/index.ts'
 
 declare var postMessage: (message: Partial<GraylogMessage>) => void
 declare var self: DedicatedWorkerGlobalScope & typeof globalThis
+
+console.log('Symbol("events.errorMonitor") ->', Symbol("events.errorMonitor"))
 
 const listener = Deno.listen({
 	hostname: '127.0.0.1',
 	port: parseInt(Deno.env.get('GRAYLOG_PORT') || '18066'),
 	transport: 'tcp',
 })
-queueMicrotask(async function onlisten() {
+
+function unload() {
+	console.warn('Graylog closing rid ->', listener.rid)
+	listener.close()
+	Deno.close(listener.rid)
+}
+
+queueMicrotask(async () => {
+	window.addEventListener('unload', () => listener.close())
+	console.info('Graylog listening ->', listener.addr)
 	for await (let conn of listener) genchunks(conn)
+
 })
 
 async function genchunks(conn: Deno.Conn) {
 	console.info('conn ->', conn.remoteAddr)
-	// let reader = io.BufReader.create(conn)
-	// mux.add(io.readStringDelim(reader, '\x00'))
-	// console.log('mux ->', mux)
-	for await (let chunk of io.readStringDelim(conn, '\x00')) {
-		// console.log('chunk ->', chunk.length)
-		// for await (let chunk of io.readStringDelim(reader, '\x00')) {
-		// console.log('message ->', message)
+	try {
+		for await (let chunk of io.readStringDelim(conn, '\x00')) {
+			console.log('chunk ->', chunk.length)
+			let message = JSON.parse(chunk) as GraylogMessage
+		}
+	} catch (error) {
+		console.error('genchunks ->', error)
+	} finally {
+		console.warn('conn close ->', conn.remoteAddr)
+		conn.close()
 	}
-	console.warn('conn close ->', conn.remoteAddr)
-	conn.close()
 }
 
-// console.log('mux ->', mux)
-// for await (let chunk of mux) {
-// 	console.log('chunk ->', chunk)
-// }
-
-// async function* genchunks(listener: Deno.Listener) {
-// 	console.info('listener ->', listener.addr)
-// 	for await (let conn of listener) {
-// 		console.info('conn ->', conn.remoteAddr)
-// 		let reader = io.BufReader.create(conn, 8192)
-// 		yield* io.readStringDelim(reader, '\x00')
-// 		// let read = await Deno.readAll(conn)
-// 		// yield* Deno.iter(conn, { bufSize: 128 })
-// 		// let buf = new Uint8Array(8192)
-// 		// let read = await conn.read(buf)
-// 		// yield read
-// 		console.warn('conn close ->', conn.remoteAddr)
-// 		conn.close()
-// 	}
-// 	console.error('listener close ->', listener.addr)
-// 	return listener.close()
-// }
-// for await (let chunk of genchunks(listener)) {
-// 	console.log('chunk ->', chunk)
-// }
-
-// async function onconn(conn: Deno.Conn) {
-// 	console.info('handle conn ->', conn.remoteAddr)
-// 	// let reader = io.BufReader.create(conn, 8192)
-// 	for await (let chunk of io.readStringDelim(conn, '\x00')) {
-// 		try {
-// 			// console.log('chunk.length ->', chunk.length)
-// 			// let message = JSON.parse(chunk) as GraylogMessage
-// 			// console.log('message ->', message)
-// 		} catch (error) {
-// 			console.error('Graylog chunk ->', chunk, error)
-// 		}
-// 	}
-// 	console.warn('handle close ->', conn.remoteAddr)
-// 	conn.close()
-// }
-
-// for await (let conn of listener) {
-
-// 	// let reader = io.BufReader.create(conn, 8192)
-// 	// io.readStringDelim(conn, '\x00')[Symbol.asyncIterator]()
-// 	// ;(async function handle(conn: Deno.Conn) {
-// 	// 	console.info('handle conn ->', conn.remoteAddr)
-// 	// 	// let reader = io.BufReader.create(conn, 8192)
-// 	// 	for await (let chunk of io.readStringDelim(conn, '\x00')) {
-// 	// 		try {
-// 	// 			// console.log('chunk.length ->', chunk.length)
-// 	// 			// let message = JSON.parse(chunk) as GraylogMessage
-// 	// 			// console.log('message ->', message)
-// 	// 		} catch (error) {
-// 	// 			console.error('Graylog chunk ->', chunk, error)
-// 	// 		}
-// 	// 	}
-// 	// 	console.warn('handle close ->', conn.remoteAddr)
-// 	// 	conn.close()
-// 	// })(conn)
-// 	// let reader = io.BufReader.create(conn, 8192)
-// 	// mux.add(io.readStringDelim(conn, '\x00'))
-// 	// console.warn('conn.close() ->')
-// 	// conn.close()
-// }
-
-// queueMicrotask(async function onconnections() {
-// 	for await (let conn of listener) {
-// 		let reader = new io.BufReader(conn)
-// 		mux.add(io.readStringDelim(conn, '\x00'))
-// 		console.warn('conn.close() ->')
-// 		conn.close()
-// 	}
-// })
-// for await (let chunk of mux) {
-// 	let message = utf8.decode(chunk)
-// 	console.log('message ->', message)
-// }
-
-// for await (let conn of listener) {
-// 	await handle(conn)
-// 	// console.info('listener connection ->', conn.remoteAddr)
-
-// 	// // let reader = io.BufReader.create(conn, 8192)
-// 	// for await (let chunk of io.readStringDelim(conn, '\x00')) {
-// 	// 	try {
-// 	// 		// console.log('chunk.length ->', chunk.length)
-// 	// 		// let message = JSON.parse(chunk) as GraylogMessage
-// 	// 		// console.log('message ->', message)
-// 	// 	} catch (error) {
-// 	// 		console.error('Graylog listener ->', chunk, error)
-// 	// 	}
-// 	// }
-
-// 	// for await (let chunk of Deno.iter(conn, { bufSize: 8192 })) {
-// 	// 	let message = utf8.decode(chunk)
-// 	// 	console.log('message ->', message)
-// 	// }
-
-// 	// console.warn('connection close ->', conn.remoteAddr)
-// 	// conn.close()
-// 	// Deno.close(conn.rid)
-// }
-
-//
-
-// const server = http.serve({
-// 	hostname: '127.0.0.1',
-// 	port: parseInt(Deno.env.get('GRAYLOG_PORT') || '18066'),
-// })
-// for await (let request of server) {
-// 	console.log('request ->', request)
-// 	try {
-// 		let message = JSON.parse(utf8.decode(await Deno.readAll(request.body))) as GraylogMessage
-// 		console.log('message ->')
-// 	} catch (error) {
-// 		console.error('Graylog request ->', error)
-// 	} finally {
-// 		await request.respond({ body: 'ok', status: 200 })
-// 	}
-// }
-
-interface GraylogMessage extends Record<string, string | number> {
+export interface GraylogMessage extends Record<string, string | number> {
 	_message_template: string
 	_SourceContext: string
 	_Type: string
@@ -171,168 +55,3 @@ interface GraylogMessage extends Record<string, string | number> {
 	timestamp: number
 	version: string
 }
-
-// interface Message extends Record<string, string | number> {
-// 	'Accept': string
-// 	'Accept-Ranges': string
-// 	'AcceptHeader': string
-// 	'Args': string
-// 	'Codecs': string
-// 	'Connection': string
-// 	'Content-Length': string
-// 	'Content-Type': string
-// 	'Date': string
-// 	'EnvVars': string
-// 	'ETag': string
-// 	'ExplicitContentTypes': string
-// 	'facility': string
-// 	'Filters': string
-// 	'host': string
-// 	'Last-Modified': string
-// 	'level': number
-// 	'ModelBinderProviders': string
-// 	'OutputFormatters': string
-// 	'Scope': string
-// 	'Server': string
-// 	'short_message': string
-// 	'timestamp': number
-// 	'Types': string
-// 	'User-Agent': string
-// 	'version': string
-// }
-
-// interface FullMessage extends Record<string, string | number> {
-// 	'_0': string
-// 	'_1': number
-// 	'_2': number
-// 	'_3': number
-// 	'_4': number
-// 	'_5': number
-// 	'_ActionId': string
-// 	'_ActionName': string
-// 	'_ActionResult': string
-// 	'_ApplicationPath': string
-// 	'_Architecture': number
-// 	'_Arguments': string
-// 	'_assemblyName': string
-// 	'_AssemblyName': string
-// 	'_AuthenticationScheme': string
-// 	'_CandidateCount': number
-// 	'_Codec': string
-// 	'_ConnectionId': string
-// 	'_ContentLength': string
-// 	'_ContentType': string
-// 	'_Controller': string
-// 	'_DueTime': string
-// 	'_Elapsed': string
-// 	'_ElapsedMilliseconds': number
-// 	'_ElapsedMs': number
-// 	'_EncoderLocation': number
-// 	'_Endpoint': string
-// 	'_EndpointName': string
-// 	'_EventId.Id': number
-// 	'_EventId.Name': string
-// 	'_ExceptionMessage': string
-// 	'_ExceptionSource': string
-// 	'_ExceptionType': string
-// 	'_ExpirationDate': string
-// 	'_FfmpegPath': string
-// 	'_Filter': string
-// 	'_FilterType': string
-// 	'_folder': string
-// 	'_FromType': string
-// 	'_FullName': string
-// 	'_FullPath': string
-// 	'_Handler': string
-// 	'_HaveSource': string
-// 	'_header': string
-// 	'_Host': string
-// 	'_HostingRequestFinishedLog': string
-// 	'_HostingRequestStartingLog': string
-// 	'_HttpHeadersLogValue': string
-// 	'_HttpMethod': string
-// 	'_Is64Bit': string
-// 	'_IsExternal': string
-// 	'_IsUserInteractive': string
-// 	'_KeyId': string
-// 	'_message_template': string
-// 	'_Method': string
-// 	'_MethodInfo': string
-// 	'_Name': string
-// 	'_ObjectResultType': string
-// 	'_origin': string
-// 	'_OS': string
-// 	'_Output': string
-// 	'_OutputFormatter': string
-// 	'_Path': string
-// 	'_path': string
-// 	'_PathBase': string
-// 	'_PhysicalPath': string
-// 	'_PluginName': string
-// 	'_PluginVersion': string
-// 	'_ProcessorCount': number
-// 	'_ProgramDataPath': string
-// 	'_Protocol': string
-// 	'_provider': string
-// 	'_Query': string
-// 	'_QueryString': string
-// 	'_RequestId': string
-// 	'_RequestPath': string
-// 	'_Result': string
-// 	'_RouteData': string
-// 	'_RoutePattern': string
-// 	'_Scheme': string
-// 	'_Source': string
-// 	'_SourceContext': string
-// 	'_StackTrace': string
-// 	'_StatusCode': number
-// 	'_stringLevel': string
-// 	'_Task': string
-// 	'_ThreadId': number
-// 	'_Time': string
-// 	'_Token': string
-// 	'_ToType': string
-// 	'_TriggerDate': string
-// 	'_Type': string
-// 	'_Uri': string
-// 	'_ValidationState': number
-// 	'_Version': string
-// 	'_VirtualPath': string
-// 	'_WebPath': string
-// 	'Accept': string
-// 	'Accept-Ranges': string
-// 	'AcceptHeader': string
-// 	'Args': string
-// 	'Codecs': string
-// 	'Connection': string
-// 	'Content-Length': string
-// 	'Content-Type': string
-// 	'Date': string
-// 	'EnvVars': string
-// 	'ETag': string
-// 	'ExplicitContentTypes': string
-// 	'facility': string
-// 	'Filters': string
-// 	'full_message': string
-// 	'host': string
-// 	'Last-Modified': string
-// 	'level': number
-// 	'ModelBinderProviders': string
-// 	'OutputFormatters': string
-// 	'Scope': string
-// 	'Server': string
-// 	'short_message': string
-// 	'timestamp': number
-// 	'Types': string
-// 	'User-Agent': string
-// 	'version': string
-// }
-
-// const socket = Deno.listenDatagram({
-// 	port: 18066,
-// 	transport: 'udp',
-// })
-// for await (let [data, address] of socket) {
-// 	console.log('address ->', address)
-// 	console.log('data ->', data.toString())
-// }
