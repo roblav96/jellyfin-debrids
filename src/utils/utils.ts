4247b5e744671a9802164b651ab74c7b900a0eb6
Robert Laverty <roblav96@gmail.com>
2021-03-02 15:33:49 -0500

    added utils/allEntries

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,18 +1,21 @@
 import * as what from 'https://deno.land/x/is_what/src/index.ts'
 
-export function allKeys(object: object) {
+export function allKeys<T>(object: T) {
 	let keys = new Set<string>()
 	let ignore = new Set(['__proto__', 'constructor', 'prototype'])
 	do {
 		let properties = Object.getOwnPropertyNames(object).concat(
-			Object.getOwnPropertySymbols(object).map((v) => v.toString()),
+			(Object.getOwnPropertySymbols(object) as any) as string[],
 		)
 		for (let property of properties) {
 			if (ignore.has(property)) continue
-			keys.add(property)
+			if (property in object) keys.add(property)
 		}
 	} while ((object = Object.getPrototypeOf(object)) && object != Object.prototype)
-	return [...keys]
+	return Array.from(keys) as (keyof T)[]
+}
+export function allEntries<T>(object: T) {
+	return allKeys(object).map((key) => [key, object[key]]) as [keyof T, T[keyof T]][]
 }
 
 export function nonce() {
