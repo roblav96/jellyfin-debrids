7ad50a82e3b1e7a0b26c5dd2140ca646fb73740f
Robert Laverty <roblav96@gmail.com>
2021-03-02 12:10:08 -0500

    added utils/allKeys

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,1 +1,20 @@
-import * as path from 'https://deno.land/std/path/mod.ts'
+import * as what from 'https://deno.land/x/is_what/src/index.ts'
+
+export function allKeys(object: object) {
+	let keys = new Set<string>()
+	let ignore = new Set(['__proto__', 'constructor', 'prototype'])
+	do {
+		let properties = Object.getOwnPropertyNames(object).concat(
+			Object.getOwnPropertySymbols(object).map((v) => v.toString()),
+		)
+		for (let property of properties) {
+			if (ignore.has(property)) continue
+			keys.add(property)
+		}
+	} while ((object = Object.getPrototypeOf(object)) && object != Object.prototype)
+	return [...keys]
+}
+
+export function nonce() {
+	return Math.random().toString(36).slice(-8)
+}
