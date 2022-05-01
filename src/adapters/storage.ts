f05b9d82a1d8f4e303ad25c3731b6e67777f6b8d
Robert Laverty <roblav96@gmail.com>
2022-05-01 08:10:43 -0400

    fixed storage entries method

--- src/adapters/storage.ts
+++ src/adapters/storage.ts
@@ -1,5 +1,6 @@
 import * as fs from 'https://deno.land/std/fs/mod.ts'
 import * as path from 'https://deno.land/std/path/mod.ts'
+import * as what from 'https://deno.land/x/is_what/src/index.ts'
 import cache_dir from 'https://deno.land/x/dir/cache_dir/mod.ts'
 
 export default class Db<T extends unknown> {
@@ -13,31 +14,22 @@ export default class Db<T extends unknown> {
 
 	private dirpath: string
 	private keypath(key: string) {
-		return path.join(this.dirpath, `_${key}_`.replaceAll(/\W/g, '_'))
+		return path.join(this.dirpath, key.replaceAll(/\W/g, '_'))
 	}
 
 	constructor(namespace: string, private ttl?: number) {
 		if (namespace.startsWith('file:')) {
 			namespace = Db.fromFileURL(namespace)
 		}
-		if (typeof ttl == 'number') {
-			this.ttl = Math.abs(ttl)
-		}
-		this.dirpath = path.join(
-			cache_dir()!,
-			'jellyfin-debrids',
-			`_${namespace}_`.replaceAll(/\W/g, '_'),
-		)
-		fs.ensureDirSync(this.dirpath)
+		this.dirpath = path.join(cache_dir()!, 'jellyfin-debrids', namespace.replaceAll(/\W/g, '_'))
 	}
 
 	async clear() {
-		await Deno.remove(this.dirpath, { recursive: true })
-		await fs.ensureDir(this.dirpath)
+		await Deno.remove(this.dirpath, { recursive: true }).catch(() => {})
 	}
 
 	async delete(key: string) {
-		await Deno.remove(this.keypath(key))
+		await Deno.remove(this.keypath(key)).catch(() => {})
 	}
 
 	async has(key: string) {
@@ -46,11 +38,9 @@ export default class Db<T extends unknown> {
 
 	async get<TT = T>(key: string) {
 		let data = await Deno.readTextFile(this.keypath(key)).catch(() => {})
-		if (typeof data != 'string') {
-			return
-		}
+		if (!what.isString(data)) return
 		let [value, ttl] = JSON.parse(data) as [TT, number?]
-		if (typeof ttl == 'number' && Date.now() > ttl) {
+		if (what.isPositiveNumber(ttl) && Date.now() > ttl) {
 			await this.delete(key)
 		} else {
 			return value
@@ -58,18 +48,22 @@ export default class Db<T extends unknown> {
 	}
 
 	async set<TT = T>(key: string, value: TT, ttl?: number) {
-		if (typeof ttl == 'number') {
-			ttl = Date.now() + Math.abs(ttl)
-		}
-		await Deno.writeTextFile(this.keypath(key), JSON.stringify([value, ttl]))
+		if (!what.isPositiveNumber(ttl) && what.isPositiveNumber(this.ttl)) ttl = this.ttl
+		await fs.ensureDir(this.dirpath)
+		await Deno.writeTextFile(
+			this.keypath(key),
+			JSON.stringify(what.isPositiveNumber(ttl) ? [value, Date.now() + ttl] : [value]),
+		)
 	}
 
 	async entries<TT = T>() {
 		let entries = [] as [string, TT][]
-		for await (let entry of Deno.readDir(this.dirpath)) {
-			let value = await this.get<TT>(entry.name)
-			value && entries.push([entry.name, value])
-		}
+		try {
+			for await (let entry of Deno.readDir(this.dirpath)) {
+				let value = await this.get<TT>(entry.name)
+				if (value) entries.push([entry.name, value])
+			}
+		} catch {}
 		return entries
 	}
 	async keys() {
